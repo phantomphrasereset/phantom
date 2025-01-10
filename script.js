f5f8cbb720aff7bbf9e349effc17f92672edc491
phantomphrasereset <resolve.phantomreset@gmail.com>
2025-01-10 06:22:40 -0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToogle = document.getElementById('menu-toogle');
-const navList = document.querySelector('.nav-list');
-
-menuToogle.addEventListener('click', () => {
-    navList.classList.toggle('show');
-});
